2bc6f24e1c2b58bd0c0c1ce64f56378ddbb853d5
Tomas Costantino <tomascostantino.au@gmail.com>
2026-05-24 21:41:12 -0300

    add: engine

--- lib/action_passkey.rb
+++ lib/action_passkey.rb
@@ -1,8 +1,26 @@
 # frozen_string_literal: true
 
 require_relative "action_passkey/version"
+require_relative "action_passkey/engine"
 
 module ActionPasskey
   class Error < StandardError; end
-  # Your code goes here...
+
+  class << self
+    attr_accessor :configuration
+
+    def configure
+      self.configuration ||= Configuration.new
+      yield configuration
+    end
+  end
+
+  class Configuration
+    attr_accessor :origins, :name
+
+    def initialize
+      @origins = []
+      @name = "My Application"
+    end
+  end
 end
---
+++ lib/action_passkey/engine.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require "active_support/core_ext/module/delegation"
+require "rails/engine"
+
+module ActionPasskey
+  class Engine < Rails::Engine
+  end
+end
